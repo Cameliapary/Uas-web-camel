cfb43b38aeb083c92e3e8fddd4c19633e8f8cd68
Cameliapary <cameliaparry54@gmail.com>
2023-02-15 12:45:14 +0700

    Uas-web-camel

---
+++ public/content/galeri.php
@@ -0,0 +1,24 @@
+<header class="container bg-warning  text-white">
+        <div class="row">
+            <div class="col-12 py-2 text-center">
+                <h2 class="display-6 ">Galleri </h2>
+            </div>
+        </div>
+    </header>
+<main class="container border p-md-5 p-2">
+        <div class="row g-2 g-md-4">
+            <div class="col-4 col-md-3">
+                <img class="w-100" src="camel.jpg" alt="Camelia">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="camel1.jpg" alt="Camelia">
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="camel.jpg" alt="Camelia"> 
+            </div>
+            <div class="col-6 col-md-3">
+                <img class="w-100" src="camel1.jpg" alt="Camelia">
+            </div>
+            
+        </div>
+    </main>
